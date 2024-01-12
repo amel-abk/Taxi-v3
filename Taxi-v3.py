4dc3d90a4ca06fd4ee2798c0f7c9f78c93f70ec4
Amel Abdelkader <abdelkaderamel@gmail.com>
2024-01-12 17:51:59 +0100

    modified_version

--- Taxi-v3.py
+++ Taxi-v3.py
@@ -1,52 +1,67 @@
 import gym
 import numpy as np
-env = gym.make("Taxi-v3", render_mode='human').env
-env.reset()
-env.render()
 
-'''print("Action Space{}".format(env.action_space))
-print("State Space{}".format(env.observation_space))'''
+# Create the Taxi-v3 environment
+env = gym.make("Taxi-v3", render_mode="human")
 
-#print("Action Space", (env.action_space.n))
-
-'''env.s =114
-env.render()
-
-
-state, reward, done, info, _= env.step(1)
-print('state:', state, 'reward:', reward,'done:', done,'info:', info)
-env.render()'''
-
-
-'''#randomly
-state= env.reset()
-counter=0
-reward= None
-while reward != 20:
-    next_state, reward, done, info, _= env.step(env.action_space.sample())
-    counter += 1
-    env.render()
-
-print(counter)'''
-
-
+# Initialize Q-table with zeros
 Q = np.zeros([env.observation_space.n, env.action_space.n])
-G=0
-alpha= 0.618
 
+# Hyperparameters
+alpha = 0.01  # learning rate
+gamma = 0.9  # discount factor
+epsilon = 0.1  # exploration-exploitation 
 
-for episode in range(1, 1001):
-    done= False
-    G, reward= 0,0
-    state, _= env.reset()
-    #print(state)
-    #break
-    while done != True:
-        action= np.argmax(Q[state])
-        next_state, reward, done, info, _ = env.step(action)
-        Q[state, action] += alpha * (reward + np.max([next_state])- Q[state, action])
-        G += reward
+# Number of episodes for training
+num_episodes = 10000
+
+# Training loop
+for episode in range(num_episodes):
+    state, _ = env.reset()
+    total_reward = 0
+    done = False
+    
+    
+    while not done:
+        # Exploration-exploitation
+        if np.random.uniform(0, 1) < epsilon:
+            # Explore: choose a random action
+            action = env.action_space.sample()  
+        else:
+            # Exploit: choose the action with the highest Q-value
+            action = np.argmax(Q[state])  
+        
+        next_state, reward, done, _, _ = env.step(action)  # Take the chosen action
+        
+        # Update Q-value using the Q-learning update rule
+        Q[state, action] += alpha * (reward + gamma * np.max(Q[next_state]) - Q[state, action])
+        
+        total_reward += reward
         state = next_state
-        env.render()
-    if episode % 50 == 0:
-        print('Episode {} Total Reward: {}'.format(episode, G))
+        #env.render() 
+    
+    # Print total reward for the episode
+    if episode % 100 == 0:
+        print(f"Episode {episode}, Total Reward: {total_reward}")
+
+
+# Evaluate the trained agent
+
+num_eval_episodes = 10
+
+for _ in range(num_eval_episodes):
+    state, _ = env.reset()
+    done = False
+    episode_reward = 0 
+    
+    while not done:
+        action = np.argmax(Q[state])  
+        next_state, reward, done, _, _ = env.step(action)
+        episode_reward += reward
+        state = next_state
+        #env.render() 
+    
+    print(f'Episode Reward: {episode_reward}')
+
+# Close the environment
+env.close()
